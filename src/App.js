a7b2a6325e58b02c227a8d309c62defaf1aca3a0
Grant Pardington <grant.pardington@gmail.com>
2020-11-19 11:26:31 -0700

    edited imports with .js to attempt heroku fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
+import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
+import About from './Components/About.js';
+import Resume from './Components/Resume.js';
+import Contact from './Components/Contact.js';
+import Testimonials from './Components/Testimonials.js';
+import Portfolio from './Components/Portfolio.js';
 
 class App extends Component {
 
